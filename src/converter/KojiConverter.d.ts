4716dddaa7cc9cf944e1281d1592f4220e807435
Yuta Hashimoto <yhashimoto1984@gmail.com>
2020-04-07 13:43:19 +0900

    unnecessary files

--- src/converter/KojiConverter.d.ts
+++
@@ -1,7 +0,0 @@
-import { KojiASTNode } from "../KojiAstBuilder";
-export interface KojiConverter {
-    convert(ast: KojiASTNode): string;
-    convertChildren(children: Array<KojiASTNode | string>): string;
-    convertInline(node: KojiASTNode): string;
-    convertBlock(node: KojiASTNode): string;
-}
