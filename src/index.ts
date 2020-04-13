4124bf4114a99ed90b75bded73892e996d5b538e
Yuta Hashimoto <yuta@YutanoMacBook-Air.local>
2020-04-13 23:24:34 +0900

    style change

--- src/index.ts
+++ src/index.ts
@@ -2,65 +2,69 @@ import { KojiHTMLConverter } from "./converter/KojiHTMLConverter";
 import { KojiAstBuilder, KojiASTNode } from "./KojiAstBuilder";
 import { KojiParser } from "./KojiParser";
 import {
-    ANTLRInputStream, CommonTokenStream, ANTLRErrorListener,
-    Recognizer,
-    RecognitionException
+  ANTLRInputStream,
+  CommonTokenStream,
+  ANTLRErrorListener,
+  Recognizer,
+  RecognitionException
 } from "antlr4ts";
 import { KojiLexer } from "./KojiLexer";
 
-
 interface KojiParseError {
-    offendingSymbol: any | undefined,
-    line: number,
-    charPositionInLine: number,
-    msg: string;
+  offendingSymbol: any | undefined;
+  line: number;
+  charPositionInLine: number;
+  msg: string;
 }
 
 class AccumlateErrorListener implements ANTLRErrorListener<any> {
-    errors: KojiParseError[];
+  errors: KojiParseError[];
 
-    constructor() {
-        this.errors = [];
-    }
+  constructor() {
+    this.errors = [];
+  }
 
-    syntaxError(
-        recognizer: Recognizer<any, any>,
-        offendingSymbol: any | undefined,
-        line: number,
-        charPositionInLine: number,
-        msg: string,
-        exception: RecognitionException | undefined
-    ) {
-        console.log(offendingSymbol);
-        this.errors.push({ line, charPositionInLine, msg, offendingSymbol });
-    }
+  syntaxError(
+    recognizer: Recognizer<any, any>,
+    offendingSymbol: any | undefined,
+    line: number,
+    charPositionInLine: number,
+    msg: string,
+    exception: RecognitionException | undefined
+  ) {
+    console.log(offendingSymbol);
+    this.errors.push({ line, charPositionInLine, msg, offendingSymbol });
+  }
 }
 
-export function tokenize(input: string): { tokens: CommonTokenStream, errors: KojiParseError[]; } {
-    const is = new ANTLRInputStream(input);
-    const lexer = new KojiLexer(is);
-    const errorHandler = new AccumlateErrorListener();
-    lexer.addErrorListener(errorHandler);
-    return { tokens: new CommonTokenStream(lexer), errors: errorHandler.errors };
+export function tokenize(
+  input: string
+): { tokens: CommonTokenStream; errors: KojiParseError[] } {
+  const is = new ANTLRInputStream(input);
+  const lexer = new KojiLexer(is);
+  const errorHandler = new AccumlateErrorListener();
+  lexer.addErrorListener(errorHandler);
+  return { tokens: new CommonTokenStream(lexer), errors: errorHandler.errors };
 }
 
-export function parse(input: string): { ast: KojiASTNode, errors: KojiParseError[]; } {
-    const is = new ANTLRInputStream(input);
-    const lexer = new KojiLexer(is);
-    const ts = new CommonTokenStream(lexer);
-    const parser = new KojiParser(ts);
-    const errorHandler = new AccumlateErrorListener();
-    lexer.addErrorListener(errorHandler);
-    parser.addErrorListener(errorHandler);
-    const tree = parser.koji();
-    const visitor = new KojiAstBuilder();
-    const ast = visitor.visit(tree);
-    const errors = errorHandler.errors;
-    return { ast, errors };
+export function parse(
+  input: string
+): { ast: KojiASTNode; errors: KojiParseError[] } {
+  const is = new ANTLRInputStream(input);
+  const lexer = new KojiLexer(is);
+  const ts = new CommonTokenStream(lexer);
+  const parser = new KojiParser(ts);
+  const errorHandler = new AccumlateErrorListener();
+  lexer.addErrorListener(errorHandler);
+  parser.addErrorListener(errorHandler);
+  const tree = parser.koji();
+  const visitor = new KojiAstBuilder();
+  const ast = visitor.visit(tree);
+  const errors = errorHandler.errors;
+  return { ast, errors };
 }
 
 export function convertToHTML(ast: KojiASTNode): string {
-    const converter = new KojiHTMLConverter();
-    return converter.convert(ast);
+  const converter = new KojiHTMLConverter();
+  return converter.convert(ast);
 }
-
