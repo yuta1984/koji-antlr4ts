4c65245f3ab6ed080fcc14cf955a84e0df394b6e
Yuta Hashimoto <yuta@YutanoMac-mini.local>
2020-05-07 16:22:08 +0900

    moved indent elements to blocks

--- src/converter/block_elements/index.ts
+++ src/converter/block_elements/index.ts
@@ -1,3 +1,5 @@
 import Cover from './Cover';
-
-export default [ Cover ];
+import Indent1 from './Indent1';
+import Indent2 from './Indent2';
+import Indent3 from './Indent3';
+export default [ Indent1, Indent2, Indent3, Cover ];
