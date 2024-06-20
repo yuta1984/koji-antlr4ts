7adce8fd7f2afb8318a5d36b73b09a1de9165c4b
Yuta Hashimoto <yhashimoto1984@gmail.com>
2024-06-20 14:35:33 +0900

    code format

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,122 @@
 import { KojiHTMLConverter } from './converter/KojiHTMLConverter';
-import { KojiAstBuilder, KojiASTNode, KojiDocumentNode, KojiInlineNode } from './KojiAstBuilder';
+import {
+  KojiAstBuilder,
+  KojiDocumentNode,
+} from './KojiAstBuilder';
 import { KojiParser } from './KojiParser';
 import {
-	ANTLRInputStream,
-	CommonTokenStream,
-	ANTLRErrorListener,
-	Recognizer,
-	RecognitionException,
-	Token
+  ANTLRInputStream,
+  CommonTokenStream,
+  ANTLRErrorListener,
+  Recognizer,
+  RecognitionException,
+  Token,
 } from 'antlr4ts';
 import { KojiLexer } from './KojiLexer';
 import { KojiXMLConverter } from './converter/KojiXMLConverter';
 import { KojiDocxDocumentConverter } from './converter/KojiDocxDocumentConverter';
-import { ConversionOptions, ConversionRule } from './converter/KojiConverter';
+import {
+  ConversionOptions,
+  ConversionRule,
+} from './converter/KojiConverter';
 import inlineConversionRules from './converter/inline_rules';
 import blockConversionRules from './converter/block_rules';
-import { toBlob, toBase64String, toBuffer } from './converter/KojiDocxCompiler';
+import {
+  toBlob,
+  toBase64String,
+  toBuffer,
+} from './converter/KojiDocxCompiler';
 import { KojiLaTeXConverter } from './converter/KojiLaTeXConverter';
 
 interface KojiParseError {
-	offendingSymbol: any | undefined;
-	line: number;
-	charPositionInLine: number;
-	msg: string;
+  offendingSymbol: any | undefined;
+  line: number;
+  charPositionInLine: number;
+  msg: string;
 }
 
-class AccumlateErrorListener implements ANTLRErrorListener<any> {
-	errors: KojiParseError[];
+class AccumlateErrorListener
+  implements ANTLRErrorListener<any>
+{
+  errors: KojiParseError[];
 
-	constructor() {
-		this.errors = [];
-	}
+  constructor() {
+    this.errors = [];
+  }
 
-	syntaxError(
-		recognizer: Recognizer<any, any>,
-		offendingSymbol: any | undefined,
-		line: number,
-		charPositionInLine: number,
-		msg: string,
-		exception: RecognitionException | undefined
-	) {
-		//console.log(offendingSymbol);
-		this.errors.push({ line, charPositionInLine, msg, offendingSymbol });
-	}
+  syntaxError(
+    recognizer: Recognizer<any, any>,
+    offendingSymbol: any | undefined,
+    line: number,
+    charPositionInLine: number,
+    msg: string,
+    exception: RecognitionException | undefined
+  ) {
+    //console.log(offendingSymbol);
+    this.errors.push({
+      line,
+      charPositionInLine,
+      msg,
+      offendingSymbol,
+    });
+  }
 }
 
-export function tokenize(input: string): { tokens: Token[]; errors: KojiParseError[]; } {
-	const is = new ANTLRInputStream(input);
-	const lexer = new KojiLexer(is);
-	const errorHandler = new AccumlateErrorListener();
-	lexer.addErrorListener(errorHandler);
-	const stream = new CommonTokenStream(lexer);
-	stream.fill();
-	return { tokens: stream.getTokens(), errors: errorHandler.errors };
+export function tokenize(input: string): {
+  tokens: Token[];
+  errors: KojiParseError[];
+} {
+  const is = new ANTLRInputStream(input);
+  const lexer = new KojiLexer(is);
+  const errorHandler = new AccumlateErrorListener();
+  lexer.addErrorListener(errorHandler);
+  const stream = new CommonTokenStream(lexer);
+  stream.fill();
+  return {
+    tokens: stream.getTokens(),
+    errors: errorHandler.errors,
+  };
 }
 
-export function parse(input: string): { ast: KojiDocumentNode; errors: KojiParseError[]; } {
-	const is = new ANTLRInputStream(input);
-	const lexer = new KojiLexer(is);
-	const ts = new CommonTokenStream(lexer);
-	const parser = new KojiParser(ts);
-	const errorHandler = new AccumlateErrorListener();
-	lexer.addErrorListener(errorHandler);
-	parser.addErrorListener(errorHandler);
-	const tree = parser.koji();
-	const visitor = new KojiAstBuilder();
-	const ast = visitor.visit(tree);
-	const errors = errorHandler.errors;
-	return { ast, errors };
+export function parse(input: string): {
+  ast: KojiDocumentNode;
+  errors: KojiParseError[];
+} {
+  const is = new ANTLRInputStream(input);
+  const lexer = new KojiLexer(is);
+  const ts = new CommonTokenStream(lexer);
+  const parser = new KojiParser(ts);
+  const errorHandler = new AccumlateErrorListener();
+  lexer.addErrorListener(errorHandler);
+  parser.addErrorListener(errorHandler);
+  const tree = parser.koji();
+  const visitor = new KojiAstBuilder();
+  const ast = visitor.visit(tree);
+  const errors = errorHandler.errors;
+  return { ast, errors };
 }
 
-export function convertToHTML(ast: KojiDocumentNode, options?: ConversionOptions): string {
-	const converter = new KojiHTMLConverter(options);
-	return converter.convert(ast);
+export function convertToHTML(
+  ast: KojiDocumentNode,
+  options?: ConversionOptions
+): string {
+  const converter = new KojiHTMLConverter(options);
+  return converter.convert(ast);
 }
 
-export function convertToXML(ast: KojiDocumentNode, options?: ConversionOptions): string {
-	const converter = new KojiXMLConverter(options);
-	return converter.convert(ast);
+export function convertToXML(
+  ast: KojiDocumentNode,
+  options?: ConversionOptions
+): string {
+  const converter = new KojiXMLConverter(options);
+  return converter.convert(ast);
 }
 
-export function convertToTEI(ast: KojiDocumentNode): string {
-	const xml = convertToXML(ast);
-	return `<?xml version="1.0" encoding="UTF-8"?>
+export function convertToTEI(
+  ast: KojiDocumentNode
+): string {
+  const xml = convertToXML(ast);
+  return `<?xml version="1.0" encoding="UTF-8"?>
 <?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml"
 	schematypens="http://purl.oclc.org/dsdl/schematron"?>
 <TEI xmlns="http://www.tei-c.org/ns/1.0">
@@ -104,32 +138,49 @@ export function convertToTEI(ast: KojiDocumentNode): string {
 	`;
 }
 
-export function convertToDocxDocumentXml(ast: KojiDocumentNode, options?: ConversionOptions): string {
-	const converter = new KojiDocxDocumentConverter(options);
-	return converter.convert(ast);
+export function convertToDocxDocumentXml(
+  ast: KojiDocumentNode,
+  options?: ConversionOptions
+): string {
+  const converter = new KojiDocxDocumentConverter(options);
+  return converter.convert(ast);
 }
 
-export function convertToLaTeX(ast: KojiDocumentNode, options?: ConversionOptions): string {
-	const converter = new KojiLaTeXConverter(options);
-	return converter.convert(ast);
+export function convertToLaTeX(
+  ast: KojiDocumentNode,
+  options?: ConversionOptions
+): string {
+  const converter = new KojiLaTeXConverter(options);
+  return converter.convert(ast);
 }
 
-type TypeName = "blob" | "base64" | "buffer";
-type ObjectType<T> =
-	T extends "blob" ? Blob :
-	T extends "base64" ? string :
-	T extends "buffer" ? Buffer :
-	never;
-export function convertToDocx<T extends TypeName>(ast: KojiDocumentNode, type: T): Promise<ObjectType<T>> {
-	if (type === "blob") {
-		return toBlob(ast) as Promise<ObjectType<T>>;
-	} else if (type === "base64") {
-		return toBase64String(ast) as Promise<ObjectType<T>>;
-	} else if (type === "buffer") {
-		return toBuffer(ast) as Promise<ObjectType<T>>;
-	}
+type TypeName = 'blob' | 'base64' | 'buffer';
+type ObjectType<T> = T extends 'blob'
+  ? Blob
+  : T extends 'base64'
+  ? string
+  : T extends 'buffer'
+  ? Buffer
+  : never;
+export function convertToDocx<T extends TypeName>(
+  ast: KojiDocumentNode,
+  type: T
+): Promise<ObjectType<T>> {
+  if (type === 'blob') {
+    return toBlob(ast) as Promise<ObjectType<T>>;
+  } else if (type === 'base64') {
+    return toBase64String(ast) as Promise<ObjectType<T>>;
+  } else if (type === 'buffer') {
+    return toBuffer(ast) as Promise<ObjectType<T>>;
+  }
 }
 
-export const conversionRules: { [elemName: string]: ConversionRule; } = {};
-inlineConversionRules.forEach((r) => (conversionRules[r.elemName] = r));
-blockConversionRules.forEach((r) => (conversionRules[r.elemName] = r));
+export const conversionRules: {
+  [elemName: string]: ConversionRule;
+} = {};
+inlineConversionRules.forEach(
+  (r) => (conversionRules[r.elemName] = r)
+);
+blockConversionRules.forEach(
+  (r) => (conversionRules[r.elemName] = r)
+);
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,222 +1,271 @@
 import { tokenize, parse } from '../src';
 import { expect } from 'chai';
-import { KojiASTNode, KojiBlockNode, KojiInlineNode } from '../src/KojiAstBuilder';
+import {
+  KojiASTNode,
+  KojiBlockNode,
+  KojiInlineNode,
+} from '../src/KojiAstBuilder';
 
 describe('ast', function () {
-	it('has an array of inline eleemtns', function () {
-		const ast = parse('《地名#hoge*fuga：日本橋》').ast;
-		expect(ast).to.have.property('inlines');
-		expect(ast.inlines).lengthOf(1);
-	});
+  it('has an array of inline eleemtns', function () {
+    const ast = parse('《地名#hoge*fuga：日本橋》').ast;
+    expect(ast).to.have.property('inlines');
+    expect(ast.inlines).lengthOf(1);
+  });
 
-	it('has an array of parent', function () {
-		const ast = parse('｛山田太郎（やまだたろう）｝').ast;
-		expect(ast).to.have.property('parens');
-		expect(ast.parens[1]).deep.equals({ start: 5, stop: 12 });
-		expect(ast.parens[0]).deep.equals({ start: 0, stop: 13 });
-	});
+  it('has an array of parent', function () {
+    const ast = parse('｛山田太郎（やまだたろう）｝').ast;
+    expect(ast).to.have.property('parens');
+    expect(ast.parens[1]).deep.equals({
+      start: 5,
+      stop: 12,
+    });
+    expect(ast.parens[0]).deep.equals({
+      start: 0,
+      stop: 13,
+    });
+  });
 });
 
 describe('parser', function () {
-	it('recognizes inline element', () => {
-		const result = parse('《地名#hoge*fuga*moge：日本橋》');
-		const inline = result.ast.body[0] as KojiASTNode;
-		expect(inline.children[0][0]).equal('日本橋');
-		expect(inline.id).equal('hoge');
-		expect(inline.classes).lengthOf(2);
-		expect(inline.classes[0]).equal('fuga');
-		expect(inline.classes[1]).equal('moge');
-	});
+  it('recognizes inline element', () => {
+    const result = parse('《地名#hoge*fuga*moge：日本橋》');
+    const inline = result.ast.body[0] as KojiASTNode;
+    expect(inline.children[0][0]).equal('日本橋');
+    expect(inline.id).equal('hoge');
+    expect(inline.classes).lengthOf(2);
+    expect(inline.classes[0]).equal('fuga');
+    expect(inline.classes[1]).equal('moge');
+  });
 
-	it('recognizes  block elements', () => {
-		const src = `
+  it('recognizes  block elements', () => {
+    const src = `
 		％章
 		％％節
 		％％％段落
 		`;
-		const result = parse(src);
-		const block3 = result.ast.blocks[0] as KojiBlockNode;
-		const block2 = result.ast.blocks[1] as KojiBlockNode;
-		const block1 = result.ast.blocks[2] as KojiBlockNode;
-		expect(result.ast.blocks).lengthOf(3);
-		expect(block1.name).equal('章');
-		expect(block1.level).equal(1);
-		expect(block2.name).equal('節');
-		expect(block2.level).equal(2);
-		expect(block3.name).equal('段落');
-		expect(block3.level).equal(3);
-	});
+    const result = parse(src);
+    console.log(result);
+    const block3 = result.ast.blocks[0] as KojiBlockNode;
+    const block2 = result.ast.blocks[1] as KojiBlockNode;
+    const block1 = result.ast.blocks[2] as KojiBlockNode;
+    expect(result.ast.blocks).lengthOf(3);
+    expect(block1.name).equal('章');
+    expect(block1.level).equal(1);
+    expect(block2.name).equal('節');
+    expect(block2.level).equal(2);
+    expect(block3.name).equal('段落');
+    expect(block3.level).equal(3);
+  });
 
-	describe('on handling syntactic sugars', () => {
-		it('recognizes furigana element', () => {
-			const result = parse('大日本（だいにほん｜ジャパン）');
-			const furigana = result.ast.body[0] as KojiASTNode;
-			expect(furigana.children[0][0]).equal('大日本');
-			expect(furigana.children[1][0]).equal('だいにほん');
-			expect(furigana.children[2][0]).equal('ジャパン');
-		});
+  describe('on handling syntactic sugars', () => {
+    it('recognizes furigana element', () => {
+      const result = parse(
+        '大日本（だいにほん｜ジャパン）'
+      );
+      const furigana = result.ast.body[0] as KojiASTNode;
+      expect(furigana.children[0][0]).equal('大日本');
+      expect(furigana.children[1][0]).equal('だいにほん');
+      expect(furigana.children[2][0]).equal('ジャパン');
+    });
 
-		it('skips whitespace before furigana', () => {
-			const result = parse('地震 年代記（ねんだいき）');
-			const furigana = result.ast.body[1] as KojiASTNode;
-			expect(furigana.children[0][0]).equal('年代記');
-			expect(furigana.children[1]).lengthOf(1);
-			expect(furigana.children[1][0]).equal('ねんだいき');
-		});
+    it('skips whitespace before furigana', () => {
+      const result = parse('地震 年代記（ねんだいき）');
+      const furigana = result.ast.body[1] as KojiASTNode;
+      expect(furigana.children[0][0]).equal('年代記');
+      expect(furigana.children[1]).lengthOf(1);
+      expect(furigana.children[1][0]).equal('ねんだいき');
+    });
 
-		it('also skips slash before furigana', () => {
-			const result = parse('地震／年代記（ねんだいき）');
-			const pre = result.ast.body[0] as string;
-			const furigana = result.ast.body[1] as KojiASTNode;
-			expect(pre).to.equals('地震');
-			expect(furigana.children[0][0]).equal('年代記');
-			expect(furigana.children[1]).lengthOf(1);
-			expect(furigana.children[1][0]).equal('ねんだいき');
-			expect(result.errors).to.be.empty;
-		});
+    it('also skips slash before furigana', () => {
+      const result = parse('地震／年代記（ねんだいき）');
+      const pre = result.ast.body[0] as string;
+      const furigana = result.ast.body[1] as KojiASTNode;
+      expect(pre).to.equals('地震');
+      expect(furigana.children[0][0]).equal('年代記');
+      expect(furigana.children[1]).lengthOf(1);
+      expect(furigana.children[1][0]).equal('ねんだいき');
+      expect(result.errors).to.be.empty;
+    });
 
-		it('allows kanji as furigana', () => {
-			const result = parse('十八（袷着用）日');
-			const furigana = result.ast.body[0] as KojiASTNode;
-			expect(furigana.children[0][0]).equal('十八');
-			expect(furigana.children[1]).lengthOf(1);
-			expect(furigana.children[1][0]).equal('袷着用');
-		});
+    it('allows kanji as furigana', () => {
+      const result = parse('十八（袷着用）日');
+      const furigana = result.ast.body[0] as KojiASTNode;
+      expect(furigana.children[0][0]).equal('十八');
+      expect(furigana.children[1]).lengthOf(1);
+      expect(furigana.children[1][0]).equal('袷着用');
+    });
 
-		it('does not put zenkaku space and kanji into the same class', () => {
-			const result = parse('　十八（じゅうはち）');
-			const space = result.ast.body[0] as string;
-			const furigana = result.ast.body[1] as KojiASTNode;
-			expect(space).to.equal('　');
-			expect(furigana.children[0][0]).equal('十八');
-		});
+    it('does not put zenkaku space and kanji into the same class', () => {
+      const result = parse('　十八（じゅうはち）');
+      const space = result.ast.body[0] as string;
+      const furigana = result.ast.body[1] as KojiASTNode;
+      expect(space).to.equal('　');
+      expect(furigana.children[0][0]).equal('十八');
+    });
 
-		it('allows furigana for ？ □　■ 【】', function () {
-			const texts = ['？（ふりがな）', '□□（ふりがな）', '■（ふりがな）', '【ホゲ】（ふりがな）'];
-			texts.forEach((text) => {
-				const result = parse(text);
-				const furigana = result.ast.body[0] as KojiASTNode;
-				expect(result.errors).to.be.empty;
-				expect(furigana.children[1][0]).to.equal('ふりがな');
-			});
-		});
+    it('allows furigana for ？ □　■ 【】', function () {
+      const texts = [
+        '？（ふりがな）',
+        '□□（ふりがな）',
+        '■（ふりがな）',
+        '【ホゲ】（ふりがな）',
+      ];
+      texts.forEach((text) => {
+        const result = parse(text);
+        const furigana = result.ast.body[0] as KojiASTNode;
+        expect(result.errors).to.be.empty;
+        expect(furigana.children[1][0]).to.equal(
+          'ふりがな'
+        );
+      });
+    });
 
-		it('recognizes okurigana and kaeriten', () => {
-			const result = parse('逐￣テ＿レ吹￣ヲ潛￣カニ');
-			const children = result.ast.body;
-			expect(children[0]).equal('逐');
-			expect(children[1]).to.have.property('name', '送り仮名');
-			expect(children[2]).to.have.property('name', '返り点');
-			expect(children[3]).equal('吹');
-			expect(children[4]).to.have.property('name', '送り仮名');
-		});
+    it('recognizes okurigana and kaeriten', () => {
+      const result = parse('逐￣テ＿レ吹￣ヲ潛￣カニ');
+      const children = result.ast.body;
+      expect(children[0]).equal('逐');
+      expect(children[1]).to.have.property(
+        'name',
+        '送り仮名'
+      );
+      expect(children[2]).to.have.property(
+        'name',
+        '返り点'
+      );
+      expect(children[3]).equal('吹');
+      expect(children[4]).to.have.property(
+        'name',
+        '送り仮名'
+      );
+    });
 
-		it('recognizes all the kaeriten', () => {
-			const kaeriten = 'レ一二三四五六七八九十上中下甲乙丙丁天地人'.split('');
-			kaeriten.forEach((k) => {
-				const src = `犬＿${k}`;
-				const ast = parse(src).ast;
-				expect(ast.body[1]).to.have.property('name', '返り点');
-			});
-		});
+    it('recognizes all the kaeriten', () => {
+      const kaeriten =
+        'レ一二三四五六七八九十上中下甲乙丙丁天地人'.split(
+          ''
+        );
+      kaeriten.forEach((k) => {
+        const src = `犬＿${k}`;
+        const ast = parse(src).ast;
+        expect(ast.body[1]).to.have.property(
+          'name',
+          '返り点'
+        );
+      });
+    });
 
-		it('recognizes tateten', () => {
-			const ast = parse('犬ー猫').ast;
-			expect(ast.body[1]).to.have.property('name', '竪点');
-		});
+    it('recognizes tateten', () => {
+      const ast = parse('犬ー猫').ast;
+      expect(ast.body[1]).to.have.property('name', '竪点');
+    });
 
-		it('recognizes annotation paren', function () {
-			const ast = parse('【コメント】').ast;
-			expect(ast.body[0]).to.have.property('name', '注釈');
-		});
+    it('recognizes annotation paren', function () {
+      const ast = parse('【コメント】').ast;
+      expect(ast.body[0]).to.have.property('name', '注釈');
+    });
 
-		it('recognizes illegible mark', function () {
-			const ast = parse('読めない■■文字').ast;
-			const illegible = ast.body[1] as KojiASTNode;
-			expect(illegible).to.have.property('name', '難読');
-			expect(illegible.children[0]).to.deep.equal(['■■']);
-		});
+    it('recognizes illegible mark', function () {
+      const ast = parse('読めない■■文字').ast;
+      const illegible = ast.body[1] as KojiASTNode;
+      expect(illegible).to.have.property('name', '難読');
+      expect(illegible.children[0]).to.deep.equal(['■■']);
+    });
 
-		it('recognizes bughole mark', function () {
-			const ast = parse('虫損している□□文字').ast;
-			const hole = ast.body[1] as KojiASTNode;
-			expect(hole).to.have.property('name', '虫損');
-			expect(hole.children[0]).to.deep.equal(['□□']);
-		});
+    it('recognizes bughole mark', function () {
+      const ast = parse('虫損している□□文字').ast;
+      const hole = ast.body[1] as KojiASTNode;
+      expect(hole).to.have.property('name', '虫損');
+      expect(hole.children[0]).to.deep.equal(['□□']);
+    });
 
-		it('recognizes person expression', function () {
-			const ast = parse('ホゲ｛山田太郎｝フガ').ast;
-			expect(ast.body[1]).to.have.property('name', '人物');
-		});
+    it('recognizes person expression', function () {
+      const ast = parse('ホゲ｛山田太郎｝フガ').ast;
+      expect(ast.body[1]).to.have.property('name', '人物');
+    });
 
-		it('recognizes date expression', function () {
-			const ast = parse('ホゲ＜令和二年五月五日＞フガ').ast;
-			expect(ast.body[1]).to.have.property('name', '日時');
-		});
+    it('recognizes date expression', function () {
+      const ast = parse('ホゲ＜令和二年五月五日＞フガ').ast;
+      expect(ast.body[1]).to.have.property('name', '日時');
+    });
 
-		it('recognizes place expression', function () {
-			const ast = parse('ホゲ〔日本橋〕フガ').ast;
-			expect(ast.body[1]).to.have.property('name', '場所');
-		});
+    it('recognizes place expression', function () {
+      const ast = parse('ホゲ〔日本橋〕フガ').ast;
+      expect(ast.body[1]).to.have.property('name', '場所');
+    });
 
-		it('recognizes attrs put after person syntactic sugar', function () {
-			const result = parse('｛山田太郎｝［＃id_string＊class_string］');
-			expect(result.errors).to.be.empty;
-			const node = result.ast.body[0];
-			expect(node).to.have.property('id', 'id_string');
-			expect(node).to.have.deep.property('classes', ['class_string']);
-		});
+    it('recognizes attrs put after person syntactic sugar', function () {
+      const result = parse(
+        '｛山田太郎｝［＃id_string＊class_string］'
+      );
+      expect(result.errors).to.be.empty;
+      const node = result.ast.body[0];
+      expect(node).to.have.property('id', 'id_string');
+      expect(node).to.have.deep.property('classes', [
+        'class_string',
+      ]);
+    });
 
-		it('recognizes attrs put after date syntactic sugar', function () {
-			const result = parse('＜一月一日＞［＃id_string＊class_string］');
-			expect(result.errors).to.be.empty;
-			const node = result.ast.body[0];
-			expect(node).to.have.property('id', 'id_string');
-			expect(node).to.have.deep.property('classes', ['class_string']);
-		});
+    it('recognizes attrs put after date syntactic sugar', function () {
+      const result = parse(
+        '＜一月一日＞［＃id_string＊class_string］'
+      );
+      expect(result.errors).to.be.empty;
+      const node = result.ast.body[0];
+      expect(node).to.have.property('id', 'id_string');
+      expect(node).to.have.deep.property('classes', [
+        'class_string',
+      ]);
+    });
 
-		it('recognizes attrs put after place syntactic sugar', function () {
-			const result = parse('〔日本橋〕［＃id_string＊class_string］');
-			expect(result.errors).to.be.empty;
-			const node = result.ast.body[0];
-			expect(node).to.have.property('id', 'id_string');
-			expect(node).to.have.deep.property('classes', ['class_string']);
-		});
+    it('recognizes attrs put after place syntactic sugar', function () {
+      const result = parse(
+        '〔日本橋〕［＃id_string＊class_string］'
+      );
+      expect(result.errors).to.be.empty;
+      const node = result.ast.body[0];
+      expect(node).to.have.property('id', 'id_string');
+      expect(node).to.have.deep.property('classes', [
+        'class_string',
+      ]);
+    });
 
-		it('allows any symbols in annotation', () => {
-			const result = parse('【コメントの中には何でも■？□はいります】');
-			console.log(result.errors);
-			expect(result.errors).to.be.empty;
-		});
+    it('allows any symbols in annotation', () => {
+      const result = parse(
+        '【コメントの中には何でも■？□はいります】'
+      );
+      console.log(result.errors);
+      expect(result.errors).to.be.empty;
+    });
 
-		it('annotation mode terminates on line break', () => {
-			const result = parse('【改行前\n改行後');
-			console.log(result.errors);
-			const node = result.ast.body[0];
-			expect(result.errors).to.be.empty;
-			expect(node).to.have.property('name', '注釈');
-		});
+    it('annotation mode terminates on line break', () => {
+      const result = parse('【改行前\n改行後');
+      console.log(result.errors);
+      const node = result.ast.body[0];
+      expect(result.errors).to.be.empty;
+      expect(node).to.have.property('name', '注釈');
+    });
 
-		it('annotation mode terminates on EOF', () => {
-			const result = parse('【コメント');
-			console.log(result.errors);
-			const node = result.ast.body[0];
-			expect(result.errors).to.be.empty;
-			expect(node).to.have.property('name', '注釈');
-		});
+    it('annotation mode terminates on EOF', () => {
+      const result = parse('【コメント');
+      console.log(result.errors);
+      const node = result.ast.body[0];
+      expect(result.errors).to.be.empty;
+      expect(node).to.have.property('name', '注釈');
+    });
 
-		it('ignores slash', () => {
-			const result = parse('1/3');
-			console.log(result.errors);
-			const node = result.ast.body[0];
-			expect(result.errors).to.be.empty;
-		});
+    it('ignores slash', () => {
+      const result = parse('1/3');
+      console.log(result.errors);
+      const node = result.ast.body[0];
+      expect(result.errors).to.be.empty;
+    });
 
-		it('ignores dots', () => {
-			const result = parse('3.14');
-			console.log(result.errors);
-			const node = result.ast.body[0];
-			expect(result.errors).to.be.empty;
-		});
-	});
+    it('ignores dots', () => {
+      const result = parse('3.14');
+      console.log(result.errors);
+      const node = result.ast.body[0];
+      expect(result.errors).to.be.empty;
+    });
+  });
 });
